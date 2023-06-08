3b9f9bbdeba58372c95b3ae62367f00bc8048a3e
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-07 23:15:25 -0600

    user global state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,15 @@
 
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { increment, decrement } from "@/redux/features/counterSlice";
+import { useGetUsersQuery } from "@/redux/services/userApi";
 
 function HomePage() {
   const count = useAppSelector((state) => state.counterReducer.counter);
+  const { data, error, isLoading, isFetching } = useGetUsersQuery(null);
+
+  if (isLoading || isFetching) return <h1>Loading...</h1>;
+  if (error) return <h1>Error</h1>;
+
   const dispatch = useAppDispatch();
 
   return (
@@ -14,6 +20,14 @@ function HomePage() {
       <button onClick={() => dispatch(increment())}>Increment</button>
       <br />
       <button onClick={() => dispatch(decrement())}>Decrement</button>
+
+      {data?.map((user) => (
+        <div key={user.id}>
+          <p>{user.name}</p>
+          <p>{user.username}</p>
+          <p>{user.email}</p>
+        </div>
+      ))}
     </>
   );
 }
--- src/redux/services/userApi.ts
+++ src/redux/services/userApi.ts
@@ -22,4 +22,4 @@ export const userApi = createApi({
   }),
 });
 
-const { useGetUsersQuery, useGetUserByIdQuery } = userApi;
+export const { useGetUsersQuery, useGetUserByIdQuery } = userApi;
